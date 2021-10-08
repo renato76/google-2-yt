39a5ba75a723686ec2894cd5cc38af800484664a
renato76 <renmignogna@gmail.com>
2021-10-08 23:26:28 +0100

    add avatar, search icon and moc icon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar';
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -8,25 +12,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     {/* Header */}
-    <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+    <header className="flex w-full px-6 py-3 justify-between text-sm text-gray-700">
       {/* left */}
       <div className="flex space-x-4 items-center">
         <p className="link">About</p>
-        <p>Store</p>
+        <p className="link">Store</p>
       </div>
 
       {/* right  */}
       <div className="flex space-x-4 items-center">
-        <p>Gmail</p>
-        <p>Images</p>
+        <p className="link">Gmail</p>
+        <p className="link">Images</p>
 
         {/* icon */}
+        <ViewGridIcon className="h-10 w-10 p-2
+        rounded-full hover:bg-gray-100 cursor-pointer " />
 
-        {/* avatar */}
+        <Avatar url="https://i2.wp.com/www.cssscript.com/wp-content/uploads/2020/12/Customizable-SVG-Avatar-Generator-In-JavaScript-Avataaars.js.png?fit=438%2C408&ssl=1" />
       </div>
     </header>
 
     {/* Body */}
+    <form className="flex flex-col mt-10 items-center">
+      <Image 
+        src="https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" 
+        alt="" 
+        height={90} 
+        width={270} 
+      />
+      <div className="flex w-full mt-5 hover:shadow-lg 
+      focus-within:shadow-lg max-w-md rounded-full border 
+      border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl" >
+        <SearchIcon className="h-5" mr-3 text-gray-500 />
+        <input type="text" className="flex-grow focus:outline-none" />
+        <MicrophoneIcon className="h-5" />
+      </div>
+    </form>
 
     {/* Footer */}
 
